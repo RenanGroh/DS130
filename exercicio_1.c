8294cb187b3cd969d6996fed320bd88db182fba2
Renan Groh <renangroh@gmail.com>
2026-03-10 12:28:17 -0300

    Exercicio 1

---
+++ exercicio_1.c
@@ -0,0 +1,10 @@
+/*
+Escrever um programa que declara três variáveis int (a, b e c). Ler um valor
+maior que zero para cada variável (se o valor digitado não é válido, mostrar
+mensagem e ler novamente). Exibe o menor valor lido multiplicado pelo maior e o
+maior valor dividido pelo menor.
+*/
+
+#include <stdio.h>
+
+int main() { int a, b, c = 0; }
